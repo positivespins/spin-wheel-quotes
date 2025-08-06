f6a12e7061203c83eaca25c125991e7de3acd04f
positivespins <mmorfios@gmail.com>
2025-08-06 15:14:28 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,27 @@
 const cacheName = 'motivational-pwa-v1';
 const assetsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/confetti.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './confetti.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+    caches.open(cacheName).then(cache =>
+      cache.addAll(assetsToCache).catch(err => {
+        console.warn('⚠️ Some files failed to cache:', err);
+      })
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
